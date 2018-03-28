6f3fcc3f3ea817faa060b33b822f6bbcf50f4ed9
kokoswang <36018262+kokoswang@users.noreply.github.com>
2018-03-27 22:48:09 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function onMouseOut(x, url) {
 	x.src = url;
 }
 
-// function onMouseOut(x) {
-//     x.style.height = "32px";
-//     x.style.width = "32px";
-// }
+function onMouseOut(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+}
